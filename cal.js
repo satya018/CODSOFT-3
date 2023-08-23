dc5895fe01bded07782673b3ca483f5d0c851cc0
Satyabrata sahoo <115993212+satya018@users.noreply.github.com>
2023-08-23 15:44:23 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,12 @@
+
+function dis(val) {
+    document.getElementById("edu").value += val
+}
+function solve() {
+    let x = document.getElementById("edu").value
+    let y = eval(x)
+    document.getElementById("edu").value = y
+}
+function clr() {
+    document.getElementById("edu").value = ""
+}
